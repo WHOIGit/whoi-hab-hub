c1122fd9a00db4c1d6796f873980202c89cbe5b3
Ethan Andrews <eandrews@whoi.edu>
2019-01-07 15:43:29 -0500

    Initial ESP Deployment model and migrations

--- habmap/esp_instrument/admin.py
+++ habmap/esp_instrument/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import EspInstrument
+from .models import EspInstrument, Deployment
 
 # Register your models here.
 
 admin.site.register(EspInstrument)
+
+admin.site.register(Deployment)
--- habmap/esp_instrument/models.py
+++ habmap/esp_instrument/models.py
@@ -1,6 +1,9 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+# ESP Models
 
 class EspInstrument(models.Model):
     esp_name = models.CharField(max_length=100)
@@ -10,3 +13,18 @@ class EspInstrument(models.Model):
 
     def __str__(self):
         return self.esp_name
+
+
+class Deployment(models.Model):
+    espinstrument = models.ForeignKey(EspInstrument, related_name='deployment',
+                                on_delete=models.CASCADE, null=False)
+    latitude = models.DecimalField(max_digits=10, decimal_places=7)
+    longitude = models.DecimalField(max_digits=10, decimal_places=7)
+    mooring_name = models.CharField(max_length=100)
+    year = models.IntegerField(validators=[MinValueValidator(1984), MaxValueValidator(datetime.date.today().year)])
+
+    class Meta:
+        ordering = ['year']
+
+    def __str__(self):
+        return '%s - %s - %s' % (self.year, self.espinstrument.esp_name, self.mooring_name)
