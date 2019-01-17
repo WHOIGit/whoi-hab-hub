363a99c331f0094e3acb8da902b303c4f8674388
ewandrews <ethan@hifrontier.com>
2019-01-17 13:02:54 -0500

    Add overlay controls to map

--- habmap/stations/views.py
+++ habmap/stations/views.py
@@ -26,6 +26,6 @@ class StationListView(ListView):
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
+        # Add in a QuerySet of all the ESP Deployments
         context['esp_deployments'] = Deployment.objects.all()
         return context
