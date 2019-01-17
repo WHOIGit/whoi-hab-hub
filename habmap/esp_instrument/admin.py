119bc51028307e00303af2a30af1329a3bc3a5da
ewandrews <ethan@hifrontier.com>
2019-01-17 13:51:11 -0500

    Create ESP Datapoint model, add to admin with import ability

--- habmap/esp_instrument/admin.py
+++ habmap/esp_instrument/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from .models import EspInstrument, Deployment
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
+
+from .models import EspInstrument, Deployment, EspDatapoint
 
 # Register your models here.
 
 admin.site.register(EspInstrument)
 
 admin.site.register(Deployment)
+
+@admin.register(EspDatapoint)
+class EspDatapointAdmin(ImportExportModelAdmin):
+    pass
+
+class EspDatapointResource(resources.ModelResource):
+
+    class Meta:
+        model = EspDatapoint
--- habmap/esp_instrument/models.py
+++ habmap/esp_instrument/models.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.db import models
+from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # ESP Models
@@ -28,3 +29,22 @@ class Deployment(models.Model):
 
     def __str__(self):
         return '%s - %s - %s' % (self.year, self.espinstrument.esp_name, self.mooring_name)
+
+
+class EspDatapoint(models.Model):
+    ALGAL_SPECIES = (
+        ('Alexandrium fundyense', 'Alexandrium fundyense'),
+        ('Pseudonitzschia', 'Pseudonitzschia'),
+    )
+
+    deployment = models.ForeignKey(Deployment, related_name='esp_datapoint',
+                                on_delete=models.CASCADE, null=False)
+    measurement = models.CharField(max_length=20, null=False, blank=True)
+    measurement_date = models.DateTimeField(default=now, null=False)
+    algal_species = models.CharField(max_length=50, choices=ALGAL_SPECIES, null=False, blank=True)
+
+    class Meta:
+        ordering = ['-measurement_date']
+
+    def __str__(self):
+        return '%s - %s' % (self.deployment.mooring_name, self.measurement_date)
