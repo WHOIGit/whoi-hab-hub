a34953e6b1067bf9727ad80cc1f73a275a823cef
Ethan Andrews <eandrews@whoi.edu>
2019-08-26 15:25:58 -0400

    Update ClosureArea admin to use Leaflet form widget for geojson field

--- habhub/closures/admin.py
+++ habhub/closures/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from leaflet.admin import LeafletGeoAdmin
 
 from .models import ClosureArea
 
 # Register your models here.
 
-admin.site.register(ClosureArea)
+admin.site.register(ClosureArea, LeafletGeoAdmin)
