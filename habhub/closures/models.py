ec441ab7773df531702a1d26d7a1f1992f171008
Ethan Andrews <eandrews@whoi.edu>
2019-08-26 14:45:58 -0400

    Update ClosureArea model, views for by state and by status, add Mapbox GL

--- habhub/closures/models.py
+++ habhub/closures/models.py
@@ -10,12 +10,19 @@ class ClosureArea(models.Model):
         ('NH', 'New Hampshire'),
     )
 
+    CURRENT_STATUS = (
+        ('Open', 'Open'),
+        ('Closed', 'Closed'),
+        ('Warning', 'Warning'),
+    )
+
     name = models.CharField(max_length=100)
     state = models.CharField(max_length=50, choices=STATES, null=False, blank=True)
     geom = PolygonField()
     acres = models.DecimalField(max_digits=19, decimal_places=10, null=True)
-    area_description = models.CharField(max_length=1000, null=True, blank=True)
-    area_class = models.CharField(max_length=100, null=True, blank=True)
+    area_description = models.CharField(max_length=1000, null=False, blank=True)
+    area_class = models.CharField(max_length=100, null=False, blank=True)
+    current_status = models.CharField(max_length=100, choices=CURRENT_STATUS, null=False, blank=True, default='Open')
 
     class Meta:
         ordering = ['state', 'name']
--- habhub/closures/views.py
+++ habhub/closures/views.py
@@ -1,11 +1,51 @@
 from django.shortcuts import render
-from django.views.generic import View, DetailView, ListView
+from django.views.generic import View, DetailView, ListView, TemplateView
+from djgeojson.views import GeoJSONResponseMixin
 
 from .models import ClosureArea
 # Create your views here.
 
 
-class ClosureHomeView(ListView):
-    model = ClosureArea
+class ClosureHomeView(TemplateView):
     template_name = 'closures/closures_home.html'
     context_object_name = 'closures'
+
+
+class ClosureHomeView2(TemplateView):
+    template_name = 'closures/closures_home2.html'
+    context_object_name = 'closures'
+
+
+class ClosureHomeView3(TemplateView):
+    template_name = 'closures/closures_home3.html'
+    context_object_name = 'closures'
+
+
+class ClosureAreaAjaxGeoLayerByStateView(GeoJSONResponseMixin, ListView):
+    model = ClosureArea
+    properties = ['name']
+
+    def get_queryset(self):
+        state_code = self.kwargs['state_code']
+        queryset = ClosureArea.objects.filter(state=state_code)
+        return queryset
+
+
+class ClosureAreaAjaxGeoLayerSingleView(GeoJSONResponseMixin, ListView):
+    model = ClosureArea
+    properties = ['name']
+
+    def get_queryset(self):
+        feature_id = self.kwargs['pk']
+        queryset = ClosureArea.objects.filter(id=feature_id)
+        return queryset
+
+
+class ClosureAreaAjaxGeoLayerByStatusView(GeoJSONResponseMixin, ListView):
+    model = ClosureArea
+    properties = ['name']
+
+    def get_queryset(self):
+        current_status = self.kwargs['current_status']
+        queryset = ClosureArea.objects.filter(current_status=current_status)
+        return queryset
