cb57e52b7738547e029340ee9abf3d5a32f2110b
Ethan Andrews <eandrews@whoi.edu>
2019-08-27 09:42:00 -0400

    Initial ClosureNotice model build

--- habhub/closures/admin.py
+++ habhub/closures/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from leaflet.admin import LeafletGeoAdmin
 
-from .models import ClosureArea
+from .models import ClosureArea, Species, ClosureNotice
 
 # Register your models here.
 
 admin.site.register(ClosureArea, LeafletGeoAdmin)
+
+admin.site.register(Species)
+
+admin.site.register(ClosureNotice)
--- habhub/closures/models.py
+++ habhub/closures/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 from djgeojson.fields import PointField, PolygonField
 
 # Create your models here.
@@ -29,3 +32,40 @@ class ClosureArea(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Species(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'Species'
+
+    def __str__(self):
+        return self.name
+
+
+class ClosureNotice(models.Model):
+    NOTICE_ACTION = (
+        ('Open', 'Open'),
+        ('Closed', 'Closed'),
+    )
+
+    SYNDROME = (
+        ('PSP', 'Paralytic Shellfish Poison (PSP)'),
+        ('DSP', 'Diarrhetic shellfish poisoning (DSP)'),
+        ('ASP', 'Amnesic shellfish poisoning (ASP)')
+    )
+
+    title = models.CharField(max_length=100)
+    date = models.DateTimeField(default=timezone.now)
+    closure_areas = models.ManyToManyField(ClosureArea)
+    species = models.ManyToManyField(Species)
+    notice_action = models.CharField(max_length=50, choices=NOTICE_ACTION, null=False, blank=True, default='Open')
+    syndrome = models.CharField(max_length=50, choices=SYNDROME, null=False, blank=True, default='PSP')
+
+    class Meta:
+        ordering = ['date', 'title']
+
+    def __str__(self):
+        return self.title
