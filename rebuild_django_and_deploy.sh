1cc5b353f0f2720b912fe61624c1004b5484cde8
Ethan Andrews <eandrews@whoi.edu>
2019-08-29 12:54:06 -0400

    Add deployment shell script

---
+++ rebuild_django_and_deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script to rebuild all Django containers and run migrate/collectstatic to deploy
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+if [ -e production.yml ]
+then
+    docker-compose -f production.yml up -d --no-deps --build django
+    docker-compose -f production.yml run --rm django python manage.py migrate
+    docker-compose -f production.yml run --rm django python manage.py collectstatic --noinput
+fi
