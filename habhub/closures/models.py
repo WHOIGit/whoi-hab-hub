f3b3b792091159b449bf4080911df9f6639ba94d
Ethan Andrews <eandrews@whoi.edu>
2019-09-16 12:27:17 -0400

    Initial Causative Organism model build

--- habhub/closures/models.py
+++ habhub/closures/models.py
@@ -44,6 +44,16 @@ class Species(models.Model):
         return self.name
 
 
+class CausativeOrganism(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
 class ClosureNotice(models.Model):
     NOTICE_ACTION = (
         ('Open', 'Open'),
