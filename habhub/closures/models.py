b13eaa7578601742de2a2d1ef6be65453186e9d7
Ethan Andrews <eandrews@whoi.edu>
2019-09-24 10:35:27 -0400

    Initial ShellfishArea Geodjango model, use leaflet in admin

--- habhub/closures/models.py
+++ habhub/closures/models.py
@@ -1,10 +1,36 @@
 import datetime
 
-from django.db import models
+from django.contrib.gis.db import models
 from django.utils import timezone
 from djgeojson.fields import PointField, PolygonField
 
-# Create your models here.
+# Closure app models.
+
+class ShellfishArea(models.Model):
+    STATES = (
+        ('ME', 'Maine'),
+        ('MA', 'Massachusetts'),
+        ('NH', 'New Hampshire'),
+    )
+
+    CURRENT_STATUS = (
+        ('Open', 'Open'),
+        ('Closed', 'Closed'),
+    )
+
+    name = models.CharField(max_length=100, db_index=True)
+    state = models.CharField(max_length=50, choices=STATES, null=False, blank=True, db_index=True)
+    geom =  models.MultiPolygonField(srid=4326)
+    acres = models.DecimalField(max_digits=19, decimal_places=10, null=True)
+    area_description = models.CharField(max_length=1000, null=False, blank=True)
+    area_class = models.CharField(max_length=100, null=False, blank=True)
+    current_status = models.CharField(max_length=100, choices=CURRENT_STATUS, null=False, blank=True, default='Open', db_index=True)
+
+    class Meta:
+        ordering = ['state', 'name']
+
+    def __str__(self):
+        return self.name
 
 class ClosureArea(models.Model):
     STATES = (
