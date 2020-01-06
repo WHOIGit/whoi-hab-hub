1524ca76ff981f9f66a802f971516a66aebdb905
Ethan Andrews <eandrews@whoi.edu>
2020-01-06 15:00:48 -0500

    Monitoring Systems US map version 1

--- habhub/monitoring_systems/urls.py
+++ habhub/monitoring_systems/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'monitoring_systems'
 urlpatterns = [
-    path('maps/us-monitoring-systems', views.MonitoringSystemMapUS.as_view(), name='monitoring_systems_map_us'),
+    path('maps/us-monitoring-systems/', views.MonitoringSystemMapUS.as_view(), name='monitoring_systems_map_us'),
+    # AJAX paths
+    path('maps/ajax/load-all-monitoring-systems/', views.MonitoringSystemAjaxGetAllView.as_view(), name='ajax_load_monitoring_systems_all'),
 ]
--- habhub/monitoring_systems/views.py
+++ habhub/monitoring_systems/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.core.serializers import serialize
 from django.views.generic import View, DetailView, ListView, TemplateView
 
 from .models import *
@@ -9,3 +10,10 @@ from .models import *
 class MonitoringSystemMapUS(TemplateView):
     template_name = 'monitoring_systems/us_map.html'
     context_object_name = 'monitoring_systems'
+
+class MonitoringSystemAjaxGetAllView(View):
+
+    def get(self, request, *args, **kwargs):
+        # Use GeoDjango geojson serializer to return JSON to map
+        ms_json = serialize('geojson', MonitoringSystem.objects.all())
+        return HttpResponse(ms_json, content_type='json')
