625dbb7ae1250e0e85fd98a2605971eaef1914eb
Ethan Andrews <eandrews@whoi.edu>
2020-01-06 12:02:08 -0500

    Update Monitoring Systems model for data import

--- habhub/monitoring_systems/models.py
+++ habhub/monitoring_systems/models.py
@@ -7,14 +7,16 @@ class MonitoringSystem(models.Model):
         ('Forecasting', 'Forecasting'),
     )
 
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=1000)
     description = models.CharField(max_length=1000, null=False, blank=True)
     geom =  models.PointField(srid=4326, null=True, blank=True)
-    system_type = MultiSelectField(choices=SYSTEM_TYPES, null=False, blank=True, default='Monitoring')
+    system_type = MultiSelectField(choices=SYSTEM_TYPES, null=False, blank=True, default='Monitoring', max_length=100)
     url = models.URLField(max_length=200, null=False, blank=True)
     location = models.CharField(max_length=100, null=False, blank=True)
     alt_url = models.URLField(max_length=200, null=False, blank=True)
     alt_location = models.CharField(max_length=100, null=False, blank=True)
+    latitude = models.DecimalField(max_digits=10, decimal_places=7, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=10, decimal_places=7, null=True, blank=True)
 
     class Meta:
         ordering = ['name']
