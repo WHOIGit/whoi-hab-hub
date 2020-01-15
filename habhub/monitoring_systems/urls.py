4e8ffab953c08ea9f2e3918bd3fd5fde4d920fce
Ethan Andrews <eandrews@whoi.edu>
2020-01-15 11:00:17 -0500

    Change map icons, add clickable system layers

--- habhub/monitoring_systems/urls.py
+++ habhub/monitoring_systems/urls.py
@@ -6,5 +6,7 @@ app_name = 'monitoring_systems'
 urlpatterns = [
     path('maps/us-monitoring-systems/', views.MonitoringSystemMapUS.as_view(), name='monitoring_systems_map_us'),
     # AJAX paths
-    path('maps/ajax/load-all-monitoring-systems/', views.MonitoringSystemAjaxGetAllView.as_view(), name='ajax_load_monitoring_systems_all'),
+    path('maps/ajax/load-all-systems/', views.MonitoringSystemAjaxGetAllView.as_view(), name='ajax_load_monitoring_systems_all'),
+    path('maps/ajax/load-all-monitoring-systems/', views.MonitoringSystemAjaxGetMonitoringView.as_view(), name='ajax_load_monitoring_systems'),
+    path('maps/ajax/load-all-forecasting-systems/', views.MonitoringSystemAjaxGetForecastingView.as_view(), name='ajax_load_forecasting_systems'),
 ]
--- habhub/monitoring_systems/views.py
+++ habhub/monitoring_systems/views.py
@@ -11,9 +11,27 @@ class MonitoringSystemMapUS(TemplateView):
     template_name = 'monitoring_systems/us_map.html'
     context_object_name = 'monitoring_systems'
 
+
+# AJAX views to get GeoJSON responses for maps
 class MonitoringSystemAjaxGetAllView(View):
 
     def get(self, request, *args, **kwargs):
         # Use GeoDjango geojson serializer to return JSON to map
         ms_json = serialize('geojson', MonitoringSystem.objects.all())
+        return HttpResponse(ms_json, content_type='json')
+
+
+class MonitoringSystemAjaxGetMonitoringView(View):
+
+    def get(self, request, *args, **kwargs):
+        # Use GeoDjango geojson serializer to return JSON to map
+        ms_json = serialize('geojson', MonitoringSystem.objects.filter(system_type__icontains='Monitoring'))
+        return HttpResponse(ms_json, content_type='json')
+
+
+class MonitoringSystemAjaxGetForecastingView(View):
+
+    def get(self, request, *args, **kwargs):
+        # Use GeoDjango geojson serializer to return JSON to map
+        ms_json = serialize('geojson', MonitoringSystem.objects.filter(system_type__icontains='Forecasting'))
         return HttpResponse(ms_json, content_type='json')
