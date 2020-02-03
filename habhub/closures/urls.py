35c77cce249f557f0a85e3131656d2214c610fbf
Ethan Andrews <eandrews@whoi.edu>
2020-02-03 11:48:38 -0500

    Update spider markers style to orange circles, AJAX feature loading works

--- habhub/closures/urls.py
+++ habhub/closures/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.ClosureHomeView.as_view(), name='closures_home'),
     path('map-status/', views.ClosureMapShapeView.as_view(), name='closures_map_shapes'),
     path('map-cluster/', views.ClosureMapClusterView.as_view(), name='closures_map_cluster'),
+    path('map-cluster2/', views.ClosureMapClusterView2.as_view(), name='closures_map_cluster2'),
     path('status/', views.ClosureHomeView2.as_view(), name='closures_home2'),
     path('status3/', views.ClosureHomeView3.as_view(), name='closures_home3'),
     # AJAX paths
@@ -15,8 +16,9 @@ urlpatterns = [
     path('maps/ajax/load-closures-state/<state_code>/', views.ClosureNoticeAjaxGetLayerByStateView.as_view(), name='ajax_load_closures_by_state'),
     path('maps/ajax/load-all-closures-points/', views.ClosureNoticeAjaxGetAllPointsView.as_view(), name='ajax_load_closures_all_points'),
     path('maps/ajax/load-closures-state-points/<state_code>/', views.ClosureNoticeAjaxGetLayerByStatePointsView.as_view(), name='ajax_load_closures_by_state_points'),
+    path('maps/ajax/load-closures-state-points-circle/<state_code>/', views.ClosureNoticeAjaxGetLayerByStatePointsCircleView.as_view(), name='ajax_load_closures_by_state_points_circle'),
     path('ajax/load-data/', views.ClosureAreaAjaxView.as_view(), name='ajax_closures_data'),
     path('ajax/load-geojson-state/<state_code>/', views.ClosureAreaAjaxGeoLayerByStateView.as_view(), name='ajax_load_closure_areas_state'),
     path('ajax/load-geojson-status/<current_status>/', views.ClosureAreaAjaxGeoLayerByStatusView.as_view(), name='ajax_load_closure_areas_status'),
-    path('ajax/load-geojson-detail/<int:pk>/', view=views.ClosureAreaAjaxGeoLayerSingleView.as_view(), name='ajax_load_closure_area'),
+    path('maps/ajax/load-geojson-detail/<int:pk>/', view=views.ClosureAreaAjaxGeoLayerSingleView.as_view(), name='ajax_load_closure_area'),
 ]
--- habhub/closures/views.py
+++ habhub/closures/views.py
@@ -8,6 +8,12 @@ from djgeojson.views import GeoJSONResponseMixin
 
 from .models import *
 
+from django.contrib.gis.db.models.functions import GeoFunc
+from django.db.models import Value
+
+class GeneratePoints(GeoFunc):
+    function='ST_GeneratePoints'
+
 ########## Functions to use within Closure CBVs ###########
 
 """
@@ -64,7 +70,7 @@ def build_closure_notice_geojson(closures_qs):
 
 """
 Function to build custom geojson objects to populate the dynamic maps.
-Returns the random Point of polygonal shellfish areas instead of shape.
+Returns the center Point of polygonal shellfish areas instead of shape.
 Parameters: 'closures_qs' - Django queryset of ClosureNotice model
 """
 def build_closure_notice_points_geojson(closures_qs):
@@ -101,6 +107,7 @@ def build_closure_notice_points_geojson(closures_qs):
                             "properties": {
                                 "title":  closure.title,
                                 "id":  closure.id,
+                                "shellfish_area_id": shellfish_area.id,
                                 "state": shellfish_area.state,
                                 "year": closure.effective_date.year,
                                 "month": closure.effective_date.month,
@@ -116,6 +123,59 @@ def build_closure_notice_points_geojson(closures_qs):
 
     return geojson_data
 
+
+"""
+Function to build custom geojson objects to populate the dynamic maps.
+Returns the Point on circle around the centroid of polygonal shellfish areas instead of shape.
+Parameters: 'closures_qs' - Django queryset of ClosureNotice model
+"""
+def build_closure_notice_circle_points_geojson(closures_qs):
+    geojson_data = {
+        'type': 'FeatureCollection',
+        'features': [],
+        'crs': {
+            'href': 'https://spatialreference.org/ref/epsg/4326/',
+            'type': 'proj4'
+        }
+    }
+
+    for closure in closures_qs:
+        for shellfish_area in closure.shellfish_areas.all().annotate(rand_point=GeneratePoints(
+                                                                'geom',
+                                                                Value(1) # to get only one point
+                                                            )):
+
+            if closure.custom_geom:
+                geom = closure.annotate(rand_point=GeneratePoints(
+                            'custom_geom',
+                            Value(1) # to get only one point
+                        ))
+
+            elif not shellfish_area.geom.empty:
+                geom = shellfish_area.rand_point
+            else:
+                geom = None
+
+            print(geom.coords)
+
+            closure_data = {"type": "Feature",
+                            "properties": {
+                                "title":  closure.title,
+                                "id":  closure.id,
+                                "state": shellfish_area.state,
+                                "year": closure.effective_date.year,
+                                "month": closure.effective_date.month,
+                                "species": [species.name for species in closure.species.all()],
+                                },
+                            "geometry": {
+                              "type": 'Point',
+                              "coordinates": geom.coords[0],
+                              }
+                            }
+            if geom:
+                geojson_data['features'].append(closure_data)
+
+    return geojson_data
 
 ######### AJAX Views to return geoJSON for maps #############
 # AJAX views to get all ClosureNotice objects for maps
@@ -166,6 +226,18 @@ class ClosureNoticeAjaxGetLayerByStatePointsView(View):
         return JsonResponse(geojson_data)
 
 
+# AJAX views to get GeoJSON responses for map layers by State code
+class ClosureNoticeAjaxGetLayerByStatePointsCircleView(View):
+
+    def get(self, request, *args, **kwargs):
+        # Get Closure notice data, format for GeoJson response
+        state_code = self.kwargs['state_code']
+        closures_qs = ClosureNotice.objects.filter(notice_action='Closed').filter(shellfish_areas__state=state_code).distinct().prefetch_related('shellfish_areas')
+        print(closures_qs.count())
+        geojson_data = build_closure_notice_circle_points_geojson(closures_qs)
+        return JsonResponse(geojson_data)
+
+
 ######### CBV Views for basic templates #############
 class ClosureMapShapeView(TemplateView):
     template_name = 'closures/closures_map_shapes.html'
@@ -174,6 +246,11 @@ class ClosureMapShapeView(TemplateView):
 
 class ClosureMapClusterView(TemplateView):
     template_name = 'closures/closures_map_cluster.html'
+    context_object_name = 'closures'
+
+
+class ClosureMapClusterView2(TemplateView):
+    template_name = 'closures/closures_map_cluster2.html'
     context_object_name = 'closures'
 
 
@@ -223,6 +300,7 @@ class ClosureAreaAjaxGeoLayerByStateView(GeoJSONResponseMixin, ListView):
 class ClosureAreaAjaxGeoLayerSingleView(GeoJSONResponseMixin, ListView):
     model = ShellfishArea
     properties = ['name']
+    #simplify = 0.0001
 
     def get_queryset(self):
         feature_id = self.kwargs['pk']
@@ -233,7 +311,7 @@ class ClosureAreaAjaxGeoLayerSingleView(GeoJSONResponseMixin, ListView):
 class ClosureAreaAjaxGeoLayerByStatusView(GeoJSONResponseMixin, ListView):
     model = ShellfishArea
     properties = ['name']
-    simplify = 0.5
+    simplify = 0.0001
 
     def get_queryset(self):
         current_status = self.kwargs['current_status']
