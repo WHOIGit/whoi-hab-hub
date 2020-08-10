49ed328a561c8b6b4b09cf64922a1202a4b5e04c
Ethan Andrews <eandrews@whoi.edu>
2020-08-10 15:37:22 -0400

    Make date filters work with either start or end date, change default past month

--- habhub/ifcb_datasets/api/views.py
+++ habhub/ifcb_datasets/api/views.py
@@ -2,6 +2,7 @@ import datetime
 
 from rest_framework import generics, viewsets
 from django_filters import rest_framework as filters
+from django.utils import timezone
 from django.db import models
 from django.db.models import Prefetch
 
@@ -16,17 +17,23 @@ class DatasetViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         queryset = Dataset.objects.all()
+        earliest_bin = Bin.objects.earliest()
         start_date = self.request.query_params.get('start_date', None)
         end_date = self.request.query_params.get('end_date', None)
 
         if start_date:
             start_date_obj = datetime.datetime.strptime(start_date, '%m/%d/%Y').date()
+        else:
+            start_date_obj = earliest_bin.sample_time
+
         if end_date:
             end_date_obj = datetime.datetime.strptime(end_date, '%m/%d/%Y').date()
+        else:
+            end_date_obj = timezone.now()
 
-        if start_date and end_date:
+        if start_date or end_date:
             queryset = queryset.prefetch_related(Prefetch(
-                'bins__species_classified',
+                'bins',
                 queryset=Bin.objects.filter(sample_time__range=[start_date_obj, end_date_obj])))
         # Set up eager loading to avoid N+1 selects
         queryset = self.get_serializer_class().setup_eager_loading(queryset)
