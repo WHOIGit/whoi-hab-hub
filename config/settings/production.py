cd03c667ad593b5a49b04dc97fa95d1c82553f8b
Ethan Andrews <eandrews@whoi.edu>
2020-10-06 12:10:44 -0400

    fix collectfast requirement

--- config/settings/production.py
+++ config/settings/production.py
@@ -143,12 +143,6 @@ ANYMAIL = {
 # ------------------------------------------------------------------------------
 INSTALLED_APPS += ['gunicorn']  # noqa F405
 
-# Collectfast
-# ------------------------------------------------------------------------------
-# https://github.com/antonagestam/collectfast#installation
-INSTALLED_APPS = ['collectfast'] + INSTALLED_APPS  # noqa F405
-AWS_PRELOAD_METADATA = True
-
 
 # LOGGING
 # ------------------------------------------------------------------------------
