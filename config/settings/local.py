eb7442a8fb3fd14f2c57a38c7051b6adf7cab60f
Ethan Andrews <eandrews@whoi.edu>
2020-10-14 19:24:09 -0400

    update django-debug toolbar settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -4,7 +4,7 @@ from .base import env
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = False
+DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='iJQDNkU81nVtwV9E6yT7ZtRocl8lscsGAWoXFD5WRLHq2UVaiOVOtni39jVHKEa0')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
@@ -57,10 +57,10 @@ INSTALLED_APPS += ['debug_toolbar']  # noqa F405
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']  # noqa F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
 DEBUG_TOOLBAR_CONFIG = {
-    'DISABLE_PANELS': [
-        'debug_toolbar.panels.redirects.RedirectsPanel',
-    ],
-    'SHOW_TEMPLATE_CONTEXT': True,
+    "DISABLE_PANELS": [
+        "debug_toolbar.panels.redirects.RedirectsPanel",
+        "debug_toolbar.panels.profiling.ProfilingPanel",],
+    "SHOW_TEMPLATE_CONTEXT": True,
 }
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = ['127.0.0.1', '10.0.2.2']
