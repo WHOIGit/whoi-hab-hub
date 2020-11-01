d4ab601d48a2bd85c058cb6ab866e98385c62525
Ethan Andrews <eandrews@whoi.edu>
2020-11-01 13:59:23 -0500

    debud prod

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,6 +8,8 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['example.com'])
 
+DEBUG = True
+
 # DATABASES
 # ------------------------------------------------------------------------------
 DATABASES['default'] = env.db('DATABASE_URL')  # noqa F405
