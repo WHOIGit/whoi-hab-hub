1e230b13534be46ade2ea7f2774a0bef400e1c08
Ethan Andrews <eandrews@whoi.edu>
2021-01-13 15:45:33 -0500

    optimize Closures api viewset queries using the Shellfish Area as key

--- habhub/closures/api/serializers.py
+++ habhub/closures/api/serializers.py
@@ -1,23 +1,60 @@
 from rest_framework import serializers
-from rest_framework_gis.serializers import GeoFeatureModelSerializer
+from rest_framework_gis.serializers import (
+    GeoFeatureModelSerializer, GeometrySerializerMethodField
+)
 
 from ..models import ClosureNotice, ClosureDataEvent, ShellfishArea
 
 
 class ShellfishAreaSerializer(GeoFeatureModelSerializer):
+    simplified_geom = GeometrySerializerMethodField()
+    closures = serializers.SerializerMethodField()
+
     class Meta:
         model = ShellfishArea
-        geo_field = 'geom'
-        fields = ['id', 'name', 'state', 'acres', 'area_description', 'geom', ]
+        geo_field = 'simplified_geom'
+        fields = ('id', 'name', 'state', 'acres', 'area_description', 'closures', 'simplified_geom',)
+
+    def get_simplified_geom(self, obj):
+        # Returns a new GEOSGeometry, simplified to the specified tolerance
+        # using the Douglas-Peucker algorithm. A higher tolerance value implies
+        # fewer points in the output. If no tolerance is provided, it
+        # defaults to 0.
+        return obj.geom.simplify(tolerance=0.0001, preserve_topology=True)
+
+    def get_closures(self, obj):
+        if obj.closure_notices.exists():
+            closures = obj.closure_notices.all()
+        else:
+            return None
+
+        closures_list = []
+        for closure in closures:
+            if closure.causative_organism:
+                causative_organism = closure.causative_organism.name
+            else:
+                causative_organism = 'Unknown'
+
+            data = {
+                "title":  closure.title,
+                "id":  closure.id,
+                "year": closure.effective_date.year,
+                "month": closure.effective_date.month,
+                "species": [species.name for species in closure.species.all()],
+                "causative_organism": causative_organism,
+                "effective_date" : closure.effective_date,
+            }
+            closures_list.append(data)
+        return closures_list
 
 
 class ClosureDataEventSerializer(GeoFeatureModelSerializer):
-    geom = serializers.SerializerMethodField('get_shellfish_area_geom')
+    geom = GeometrySerializerMethodField('get_shellfish_area_geom')
 
     class Meta:
         model = ClosureDataEvent
         geo_field = 'geom'
-        fields = ['id', 'closure_notice', 'species', 'effective_date', 'notice_action', 'duration', 'causative_organism', 'geom', ]
+        fields = ('id', 'closure_notice', 'species', 'effective_date', 'notice_action', 'duration', 'causative_organism', 'geom',)
 
     def get_shellfish_area_geom(self, obj):
-        return obj.shellfish_area.geom
+        return obj.shellfish_area.geom.simplify(tolerance=0.0001, preserve_topology=True)
--- habhub/closures/api/views.py
+++ habhub/closures/api/views.py
@@ -6,8 +6,13 @@ from django.utils import timezone
 from django.db import models
 from django.db.models import Prefetch, F
 
-from ..models import ClosureDataEvent, ShellfishArea
-from .serializers import ClosureDataEventSerializer, ShellfishAreaSerializer
+from ..models import (
+    ClosureNotice, ClosureDataEvent, ShellfishArea,
+)
+from .serializers import (
+    ClosureDataEventSerializer,
+    ShellfishAreaSerializer
+)
 
 
 class ClosureDataEventViewSet(viewsets.ReadOnlyModelViewSet):
@@ -26,4 +31,38 @@ class ShellfishAreaViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         queryset = ShellfishArea.objects.all()
+        start_date = self.request.query_params.get('start_date', None)
+        end_date = self.request.query_params.get('end_date', None)
+        earliest_closure = ClosureNotice.objects.earliest()
+
+        if start_date:
+            start_date_obj = datetime.datetime.strptime(start_date, '%m/%d/%Y').date()
+        else:
+            start_date_obj = earliest_closure.effective_date
+
+        if end_date:
+            end_date_obj = datetime.datetime.strptime(end_date, '%m/%d/%Y').date()
+        else:
+            end_date_obj = timezone.now()
+
+        if start_date or end_date:
+            # get initial list of notice IDs to only return Shellfish Areas with Closures
+            notice_date_ids = (
+                ClosureNotice.objects.filter(effective_date__range=[start_date_obj, end_date_obj], notice_action='Open')
+                                     .values('id')
+            )
+            queryset = queryset.filter(closure_notices__in=notice_date_ids).distinct()
+            # create subquery to also filter the ClosureNotice set that is prefetched
+            closure_notice_query = ClosureNotice.objects.filter(effective_date__range=[start_date_obj, end_date_obj], notice_action='Open')
+            # do the prefetching
+            queryset = queryset.prefetch_related(
+                Prefetch(
+                    'closure_notices',
+                    queryset=closure_notice_query
+                ))
+
+        else:
+            queryset = (
+                queryset.exclude(closure_notices=None).prefetch_related('closure_notices')
+            )
         return queryset
