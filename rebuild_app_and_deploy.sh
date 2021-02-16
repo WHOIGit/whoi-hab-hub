e5482343f72846411fd7dc32e4260a2fc2b0bb59
ethanandrews <eandrews@whoi.edu>
2021-02-16 09:55:30 -0500

    slimmer multi step Docker works

--- rebuild_app_and_deploy.sh
+++ rebuild_app_and_deploy.sh
@@ -4,11 +4,11 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 if [ -e production.yml ]
 then
-    docker-compose -f production.yml up -d --no-deps --build django
-    docker-compose -f production.yml up -d --no-deps --build react_frontend
-    docker-compose -f production.yml run --rm django python manage.py migrate
-    docker-compose -f production.yml run --rm django python manage.py collectstatic --noinput
-    docker-compose -f production.yml up -d --no-deps --build celeryworker
-    docker-compose -f production.yml up -d --no-deps --build celerybeat
-    docker-compose -f production.yml up -d --no-deps --build flower
+    docker-compose -f docker-compose-prod.yml up -d --no-deps --build django
+    docker-compose -f docker-compose-prod.yml up -d --no-deps --build react_frontend
+    docker-compose -f docker-compose-prod.yml run --rm django python manage.py migrate
+    docker-compose -f docker-compose-prod.yml run --rm django python manage.py collectstatic --noinput
+    docker-compose -f docker-compose-prod.yml up -d --no-deps --build celeryworker
+    docker-compose -f docker-compose-prod.yml up -d --no-deps --build celerybeat
+    docker-compose -f docker-compose-prod.yml up -d --no-deps --build flower
 fi
