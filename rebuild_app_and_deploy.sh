959abddd2bbdd6ae7472d36a04fadd4146994e5b
ethanandrews <eandrews@whoi.edu>
2021-02-17 16:28:47 -0500

    update deployment script

--- rebuild_app_and_deploy.sh
+++ rebuild_app_and_deploy.sh
@@ -2,13 +2,15 @@
 # Script to rebuild all Django containers and run migrate/collectstatic to deploy
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-if [ -e production.yml ]
+if [ -e docker-compose-prod.yml ]
 then
-    docker-compose -f docker-compose-prod.yml up -d --no-deps --build django
-    docker-compose -f docker-compose-prod.yml up -d --no-deps --build react_frontend
+    docker pull eandrewswhoi/hab-hub-django-dataserver:0.1
+    docker-compose -f docker-compose-prod.yml up -d --no-deps django
     docker-compose -f docker-compose-prod.yml run --rm django python manage.py migrate
     docker-compose -f docker-compose-prod.yml run --rm django python manage.py collectstatic --noinput
-    docker-compose -f docker-compose-prod.yml up -d --no-deps --build celeryworker
-    docker-compose -f docker-compose-prod.yml up -d --no-deps --build celerybeat
-    docker-compose -f docker-compose-prod.yml up -d --no-deps --build flower
+    docker-compose -f docker-compose-prod.yml up -d --no-deps celeryworker
+    docker-compose -f docker-compose-prod.yml up -d --no-deps celerybeat
+    docker-compose -f docker-compose-prod.yml up -d --no-deps flower
+    docker pull eandrewswhoi/hab-hub-frontend-client:latest
+    docker-compose -f docker-compose-prod.yml up -d --no-deps react_frontend
 fi
