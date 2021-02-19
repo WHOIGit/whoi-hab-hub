46e112b650270babffefa27e1de45c36b4e7c200
ethanandrews <eandrews@whoi.edu>
2021-02-19 12:45:42 -0500

    update docker

--- rebuild_app_and_deploy.sh
+++ rebuild_app_and_deploy.sh
@@ -4,13 +4,12 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 if [ -e docker-compose-prod.yml ]
 then
-    docker pull eandrewswhoi/hab-hub-django-dataserver:0.1
+    docker-compose -f docker-compose-prod.yml pull
     docker-compose -f docker-compose-prod.yml up -d --no-deps django
     docker-compose -f docker-compose-prod.yml run --rm django python manage.py migrate
     docker-compose -f docker-compose-prod.yml run --rm django python manage.py collectstatic --noinput
     docker-compose -f docker-compose-prod.yml up -d --no-deps celeryworker
     docker-compose -f docker-compose-prod.yml up -d --no-deps celerybeat
     docker-compose -f docker-compose-prod.yml up -d --no-deps flower
-    docker pull eandrewswhoi/hab-hub-frontend-client:latest
     docker-compose -f docker-compose-prod.yml up -d --no-deps react_frontend
 fi
