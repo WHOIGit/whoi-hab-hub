b1d7a1ed910279374f04c3c208b6e902a8f72a19
ethanandrews <eandrews@whoi.edu>
2021-03-14 14:49:59 -0400

    add Data Sources to Legend tab

--- habhub-react-frontend/src/components/dashboard/LegendTab.js
+++ habhub-react-frontend/src/components/dashboard/LegendTab.js
@@ -4,9 +4,13 @@ import {
   Typography,
   Link,
   Divider,
+  Grid,
 } from '@material-ui/core';
 import LegendCellConcentration from "./LegendCellConcentration"
 import LegendToxicity from "./LegendToxicity"
+import DiamondMarker from '../../images/diamond.svg';
+import CircleMarker from '../../images/circle.svg';
+import SquareMarker from '../../images/square-orange.svg';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,7 +22,10 @@ const useStyles = makeStyles((theme) => ({
   },
   legendDivider: {
     maring: theme.spacing(2)
-  }
+  },
+  layerIcon: {
+    width: "25px",
+  },
 }));
 
 export default function LegendTab({ habSpecies, renderColorChips }) {
@@ -32,11 +39,63 @@ export default function LegendTab({ habSpecies, renderColorChips }) {
 
       <div className={classes.legendBox}>
         <Typography variant="body1" display="block" gutterBottom>
+          Data Sources
+        </Typography>
+
+        <Grid container spacing={0}>
+          <Grid item xs={2}>
+            <div>
+              <img src={DiamondMarker} alt="Station Toxicity Legend Icon" className={classes.layerIcon} />
+            </div>
+          </Grid>
+          <Grid item xs={10}>
+            <Typography variant="body2" color="textSecondary" className={classes.labelText}>
+              Shellfish Toxicity (state data)
+            </Typography>
+          </Grid>
+          <Grid item xs={2}>
+            <div>
+              <img src={CircleMarker} alt="IFCB Legend Icon" className={classes.layerIcon} />
+            </div>
+          </Grid>
+          <Grid item xs={10}>
+            <Typography variant="body2" color="textSecondary" className={classes.labelText}>
+              IFCB
+            </Typography>
+          </Grid>
+          <Grid item xs={2}>
+            <div>
+              <img src="images/icon-shellfish-closure.png" alt="Closures Legend Icon" className={classes.layerIcon} />
+            </div>
+          </Grid>
+          <Grid item xs={10}>
+            <Typography variant="body2" color="textSecondary" className={classes.labelText}>
+              Shellfish Bed Closure
+            </Typography>
+          </Grid>
+          <Grid item xs={2}>
+            <div>
+              <img src={SquareMarker} alt="Closures Area Color" className={classes.layerIcon} />
+            </div>
+          </Grid>
+          <Grid item xs={10}>
+            <Typography variant="body2" color="textSecondary" className={classes.labelText}>
+              Closure Area
+            </Typography>
+          </Grid>
+        </Grid>
+
+      </div>
+      <Divider />
+
+      <div className={classes.legendBox}>
+        <Typography variant="body1" display="block" gutterBottom>
           Cell Concentration
         </Typography>
         <LegendCellConcentration />
       </div>
       <Divider />
+
       <div className={classes.legendBox}>
         <Typography variant="body1" display="block" gutterBottom>
           Shellfish Toxicity
