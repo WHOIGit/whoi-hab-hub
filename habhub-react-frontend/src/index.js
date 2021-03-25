8e06f698452b53cba9b564f2f3816a0f23be20b0
ethanandrews <eandrews@whoi.edu>
2021-03-25 11:38:12 -0400

    add Google Analytics

--- habhub-react-frontend/src/index.js
+++ habhub-react-frontend/src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import ReactGA from "react-ga";
+// Google Analytics setup
+ReactGA.initialize("G-LR7ZKFG1MX");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
