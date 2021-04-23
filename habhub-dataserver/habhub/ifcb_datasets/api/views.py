19fc76b2298a76743858775040103f0c3b7b9f6e
ethanandrews <eandrews@whoi.edu>
2021-04-23 15:04:11 -0400

    fix api error on no data

--- habhub-dataserver/habhub/ifcb_datasets/api/views.py
+++ habhub-dataserver/habhub/ifcb_datasets/api/views.py
@@ -17,22 +17,27 @@ class DatasetViewSet(viewsets.ReadOnlyModelViewSet):
     filter_backends = (filters.DjangoFilterBackend,)
 
     def get_queryset(self):
-        queryset = Dataset.objects.defer('bins')
-        earliest_bin = Bin.objects.earliest()
-        start_date = self.request.query_params.get('start_date', None)
-        end_date = self.request.query_params.get('end_date', None)
-        seasonal = self.request.query_params.get('seasonal', None) == 'true'
-        exclude_month_range = self.request.query_params.get('exclude_month_range', None) == 'true'
+        queryset = Dataset.objects.defer("bins")
+        start_date = self.request.query_params.get("start_date", None)
+        end_date = self.request.query_params.get("end_date", None)
+        seasonal = self.request.query_params.get("seasonal", None) == "true"
+        exclude_month_range = (
+            self.request.query_params.get("exclude_month_range", None) == "true"
+        )
         # integer to divide the total dataset bins by to smooth out long term graphs/improve performance
-        smoothing_factor = self.request.query_params.get('smoothing_factor', 1)
+        smoothing_factor = self.request.query_params.get("smoothing_factor", 1)
+        try:
+            earliest_bin = Bin.objects.earliest()
+        except Bin.DoesNotExist:
+            return queryset
 
         if start_date:
-            start_date_obj = datetime.datetime.strptime(start_date, '%m/%d/%Y').date()
+            start_date_obj = datetime.datetime.strptime(start_date, "%m/%d/%Y").date()
         else:
             start_date_obj = earliest_bin.sample_time
 
         if end_date:
-            end_date_obj = datetime.datetime.strptime(end_date, '%m/%d/%Y').date()
+            end_date_obj = datetime.datetime.strptime(end_date, "%m/%d/%Y").date()
         else:
             end_date_obj = timezone.now()
 
@@ -41,55 +46,86 @@ class DatasetViewSet(viewsets.ReadOnlyModelViewSet):
             # if "seaonsal" filter is True, need to get multiple date ranges across the time series
             if seasonal:
                 date_ranges = []
-                year_range = [*range(start_date_obj.year, end_date_obj.year+1)]
+                year_range = [*range(start_date_obj.year, end_date_obj.year + 1)]
 
                 for year in year_range:
                     # if exclude_month_range filter is true, need to invert the date ranges so they span the next year
                     if exclude_month_range:
-                        range_start_date = make_aware(datetime.datetime(year, end_date_obj.month , end_date_obj.day))
-                        range_end_date = make_aware(datetime.datetime(year + 1, start_date_obj.month, start_date_obj.day))
+                        range_start_date = make_aware(
+                            datetime.datetime(
+                                year, end_date_obj.month, end_date_obj.day
+                            )
+                        )
+                        range_end_date = make_aware(
+                            datetime.datetime(
+                                year + 1, start_date_obj.month, start_date_obj.day
+                            )
+                        )
                     else:
-                        range_start_date = make_aware(datetime.datetime(year, start_date_obj.month, start_date_obj.day))
-                        range_end_date = make_aware(datetime.datetime(year, end_date_obj.month, end_date_obj.day))
+                        range_start_date = make_aware(
+                            datetime.datetime(
+                                year, start_date_obj.month, start_date_obj.day
+                            )
+                        )
+                        range_end_date = make_aware(
+                            datetime.datetime(
+                                year, end_date_obj.month, end_date_obj.day
+                            )
+                        )
 
                     range_dict = {
-                        'year': year,
-                        'start_date': range_start_date,
-                        'end_date': range_end_date
+                        "year": year,
+                        "start_date": range_start_date,
+                        "end_date": range_end_date,
                     }
                     date_ranges.append(range_dict)
 
                 date_q_filters = Q()
                 for dr in date_ranges:
-                    date_q_filters |= Q(sample_time__range=(dr['start_date'], dr['end_date'])) # 'or' the Q objects together
+                    date_q_filters |= Q(
+                        sample_time__range=(dr["start_date"], dr["end_date"])
+                    )  # 'or' the Q objects together
 
-                queryset = queryset.prefetch_related(Prefetch(
-                    'bins',
-                    queryset=Bin.objects.filter(cell_concentration_data__isnull=False) \
-                                        .filter(date_q_filters) \
-                                        .annotate(smoothing=F('id') % smoothing_factor).filter(smoothing=0)
-                                        ))
+                queryset = queryset.prefetch_related(
+                    Prefetch(
+                        "bins",
+                        queryset=Bin.objects.filter(
+                            cell_concentration_data__isnull=False
+                        )
+                        .filter(date_q_filters)
+                        .annotate(smoothing=F("id") % smoothing_factor)
+                        .filter(smoothing=0),
+                    )
+                )
             else:
-                queryset = queryset.prefetch_related(Prefetch(
-                    'bins',
-                    queryset=Bin.objects.filter(cell_concentration_data__isnull=False) \
-                                        .filter(sample_time__range=[start_date_obj, end_date_obj]) \
-                                        .annotate(smoothing=F('id') % smoothing_factor).filter(smoothing=0)
-                                        ))
-        elif self.action == 'retrieve':
-            queryset = queryset.prefetch_related(Prefetch(
-                'bins',
-                queryset=Bin.objects.filter(cell_concentration_data__isnull=False) \
-                                    .annotate(smoothing=F('id') % smoothing_factor).filter(smoothing=0)
-                                    ))
+                queryset = queryset.prefetch_related(
+                    Prefetch(
+                        "bins",
+                        queryset=Bin.objects.filter(
+                            cell_concentration_data__isnull=False
+                        )
+                        .filter(sample_time__range=[start_date_obj, end_date_obj])
+                        .annotate(smoothing=F("id") % smoothing_factor)
+                        .filter(smoothing=0),
+                    )
+                )
+        elif self.action == "retrieve":
+            queryset = queryset.prefetch_related(
+                Prefetch(
+                    "bins",
+                    queryset=Bin.objects.filter(cell_concentration_data__isnull=False)
+                    .annotate(smoothing=F("id") % smoothing_factor)
+                    .filter(smoothing=0),
+                )
+            )
         return queryset
 
     # return different sets of fields if the request is list all or retrieve one,
     # so use two different serializers
     def get_serializer_class(self):
         print(self.action)
-        if self.action == 'retrieve':
-            if hasattr(self, 'detail_serializer_class'):
+        if self.action == "retrieve":
+            if hasattr(self, "detail_serializer_class"):
                 return self.detail_serializer_class
 
         return super(DatasetViewSet, self).get_serializer_class()
