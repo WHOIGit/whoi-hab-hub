220ec24201d3a866cb88cefdd164e1fc848624bb
ethanandrews <eandrews@whoi.edu>
2022-01-07 08:09:49 -0500

    remove GA

--- habhub-react-frontend/src/index.js
+++ habhub-react-frontend/src/index.js
@@ -1,28 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import GA4React from "ga-4-react";
 import App from "./app/App";
 
 // Redux
 import store from "./app/store";
 import { Provider } from "react-redux";
 
-// Google Analytics setup
-let GA_UID = null;
-if (process.env.REACT_APP_GA_UID) {
-  //GA_UID = process.env.REACT_APP_GA_UID;
-}
-const ga4react = new GA4React(GA_UID);
-
-(async () => {
-  await ga4react.initialize();
-
-  ReactDOM.render(
-    <Provider store={store}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </Provider>,
-    document.getElementById("root")
-  );
-})();
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
+);
