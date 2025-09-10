e22b06d1ea800163180955e2921a210950d53984
ethanandrews <eandrews@whoi.edu>
2025-09-10 15:14:51 -0400

    update CSRF_TRUSTED_ORIGINS

--- habhub-dataserver/config/settings/production.py
+++ habhub-dataserver/config/settings/production.py
@@ -54,7 +54,7 @@ SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=False)
 SESSION_COOKIE_SECURE = False
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
 CSRF_COOKIE_SECURE = False
-CSRF_TRUSTED_ORIGINS = env.list("DJANGO_ALLOWED_HOSTS", default=["whoi.edu"])
+CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
 # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
 # TODO: set this to 60 seconds first and then to 518400 once you prove the former works
